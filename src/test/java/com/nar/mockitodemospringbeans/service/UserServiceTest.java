fe4e92b335e4cb505c280e98db9f4d6179ca4950
Narendra <nknirala.0071@gmail.com>
2018-08-13 09:31:55 +0530

    Springboot Mockito example

---
+++ src/test/java/com/nar/mockitodemospringbeans/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package com.nar.mockitodemospringbeans.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private NameService nameService;
+
+    @Test
+    public void whenUserIdIsProvided_thenRetrievedNameIsCorrect() {
+        Mockito.when(nameService.getUserName("SomeId")).thenReturn("Mock user name");
+        String testName = userService.getUserName("SomeId");
+        Assert.assertEquals("Mock user name", testName);
+    }
+}
